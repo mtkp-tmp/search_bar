f78e0accb7184db20edbcc32995f6456dfe41c8a
Ismailov Maxim <35664102+actionIsComing@users.noreply.github.com>
2018-02-19 19:30:22 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -13,32 +13,18 @@ if __name__ == "__main__":
 #Черновик
 #__________________________________________________________________________________________________________
 import json
-import re
+import math
 
-# coordInp = input("Введите координаты: ")
+'''coordX = input("Введите координаты X: ")
+coordY = input("Введите координаты Y: ")'''
 
 with open('data-2897-2017-12-21.json', 'r', encoding='windows-1251') as fileJSON:
     data = json.load(fileJSON)
 
-dataFrJS = json.dumps(data, sort_keys=False, indent=4, ensure_ascii=False, separators=(',', ': '))
-print(dataFrJS)
-
-# название ближайшего бара
-# адрес бара
-# количество сидячих мест
-
-'''name = re.findall(r'"Name": "\s*([^,]+|\S+)"', dataFrJS)
-SeatsCount = re.findall(r'"SeatsCount": \s*([^,]+|\S+)', dataFrJS)
-Address = re.findall(r'"Address": "\s*([^,]+|\S+)', dataFrJS)
-
-print(name)
-print(Address)
-print(SeatsCount)'''
-
-
 print(data[0]['Name'])
-
-
-# mas = {key:search_ID_FrJS[i] for i, key in enumerate(search_GlID_FrJS)}
+print(data[0]['Address'])
+print(data[0]['SeatsCount'])
+print(data[0]['geoData']['coordinates'][0])
+print(data[0]['geoData']['coordinates'][1])
 
 fileJSON.close()
