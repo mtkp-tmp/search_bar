408cfe2bc4ca5751eea5be1f24534fe932ce6ca1
Slager99 <32762977+Slager99@users.noreply.github.com>
2018-02-21 16:02:28 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,2 +1,8 @@
 if __name__ == "__main__":
-  print('go!')
+    import  upload_data as up
+    import  calculations as cal
+    import  output_info as out
+    f = open('C:/Users/Владислав/Desktop/data-2897-2017-12-21.json', 'r')
+    Parsed_string = up.ReadJson(f)
+    Calculation = cal.CalculBar(Parsed_string)
+    Out = out.OutputBar(Parsed_string, Calculation)
