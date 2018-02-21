37958115520722d9869cab267ea497e4933edade
Slager99 <32762977+Slager99@users.noreply.github.com>
2018-02-21 15:58:02 +0300

    Update output_info.py

--- search_bar/output_info.py
+++ search_bar/output_info.py
@@ -1,1 +1,6 @@
+def OutputBar(parsed_string,index):
+    print("Название заведения: ", parsed_string[index].get('Name'))
+    print("Адрес заведения: ", parsed_string[index].get('Address'))
+    print("Телефонный номер: 8", parsed_string[index].get('PublicPhone')[0].get('PublicPhone'))
+    print("Количество сидящих мест: ", parsed_string[index].get('SeatsCount'))
 
