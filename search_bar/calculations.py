2001441012c2d8e3ddd727c0c0b455c8738f4fde
Slager99 <32762977+Slager99@users.noreply.github.com>
2018-02-21 15:56:55 +0300

    Update calculations.py

--- search_bar/calculations.py
+++ search_bar/calculations.py
@@ -1,1 +1,12 @@
+from math import sqrt
+
+def CalculBar(parsed_string):
+    x =float(input("input longitude: "))  #55.772519
+    y =float(input("input latitude: "))   #37.685202
+    lst = []
+    for i in (parsed_string):
+        formula = sqrt((i.get('geoData').get('coordinates')[1] - x)**2 + (i.get('geoData').get('coordinates')[0] - y)**2)
+        lst.append(formula)
+        index = lst.index(min(lst))
+    return index
 
