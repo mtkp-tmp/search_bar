b3615c4e1e3e1885db0fe61b8d7378435df29dc2
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-02 11:19:25 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,32 +1,9 @@
-#__________________________________________________________________________________________________________
-#Шаблон модуля main
-#__________________________________________________________________________________________________________
-import upload_data #Или |from upload_data import имя_функции| для импортирования только одной функции
-import calculations
-import output_info
-#вызовы функций из модулей:
-#имя_модуля.имя_функции()
-
-if __name__ == "__main__":
-  main()
-#__________________________________________________________________________________________________________
-#Черновик
-#__________________________________________________________________________________________________________
-import json
-import math
-
-'''coordX = input("Введите координаты X: ")
-coordY = input("Введите координаты Y: ")'''
-
-with open('data-2897-2017-12-21.json', 'r', encoding='windows-1251') as fileJSON:
-    data = json.load(fileJSON)
-
-print(data[0]['Name'])
-print(data[0]['Address'])
-print(data[0]['SeatsCount'])
-print(data[0]['geoData']['coordinates'][0])
-print(data[0]['geoData']['coordinates'][1])
-
-fileJSON.close()
-
-#Позже перенести в upload_data.py
+from json import load
+from calculations import calculations
+with open('C:\\Users\\Max\\Desktop\\data-2897-2017-12-21.json','r') as f:
+    data, shortest_way_distance, current_coords = load(f), 1999.0, { "Latitude_WGS84" : float(input("Введите долготу")), "Longitude_WGS84" : float(input("Введите широту")) }
+for element in data:
+    closest_coords = {"Latitude_WGS84" : float(element["Latitude_WGS84"]), "Longitude_WGS84" : float(element["Longitude_WGS84"]) }
+    if shortest_way_distance > calculations(current_coords["Latitude_WGS84"], current_coords["Longitude_WGS84"], closest_coords["Latitude_WGS84"], closest_coords["Longitude_WGS84"]):
+        shortest_way_distance, shortest_way = calculations(current_coords["Latitude_WGS84"], current_coords["Longitude_WGS84"], closest_coords["Latitude_WGS84"], closest_coords["Longitude_WGS84"]), { "Название ближайшего бара" : element["Name"], "Адрес бара" : element["Address"], "Количество сидячих мест" : element["SeatsCount"] }
+print(shortest_way)
