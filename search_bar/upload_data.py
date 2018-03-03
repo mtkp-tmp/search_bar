7b5c5da826c8a3b831da65c1fdb7bcbd16d81ee9
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-03 19:50:05 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -1,1 +1,8 @@
-
+from json import load as l
+from os.path import isfile as isf
+def read_bars(): # Вводимая директория файла json
+    path = ""
+    while (isf(path) !=  True) and (path[len(path)-5:len(path)] != ".json"):   # Проверка на правильность ввода директории и типа файла
+        path = input("Введите путь к файлу json") # C:\\Users\\Lenovo\\Desktop\\data-2897-2017-12-21.json
+        if isf(path) == False: print("Файл не найден, попробуйте ввести директорию файла заново")
+    with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла 
