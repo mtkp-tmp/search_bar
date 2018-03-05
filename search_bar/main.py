e536123a401eeaf3fceec3f1de6a7a64bc6d0d27
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-05 22:05:05 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,6 +1,9 @@
 import upload_data as upl, calculations as calc, output_info as out
 if __name__ == '__main__':
     data = upl.read_bars()
-    current_coords = {40,55}
+    while True: # Проверка на правильность ввода координат
+        try: current_coords = { "Latitude_WGS84" : 40.0, "Longitude_WGS84" : 55.0 }
+        except ValueError: print("Введённые координаты не могут быть преобразованый в float, попробуйте ввести координаты снова")
+        else: break
     closest_bar = calc.shortest_way(current_coords, data)
     out.output(closest_bar)
