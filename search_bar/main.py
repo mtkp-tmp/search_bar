f79e7ad6a76e56040e7ef2e5ca80263beefa3e03
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-06 18:11:53 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     data = upl.read_bars()
     while True: # Проверка на правильность ввода координат
         try: current_coords = { "Latitude_WGS84" : float(input("Введите долготу")), "Longitude_WGS84" : float(input("Введите широту")) }
-        except ValueError: print("Введённые координаты не могут быть преобразованый в float, попробуйте ввести координаты снова")
+        except ValueError: print("Введённые координаты не могут быть преобразованы в float, попробуйте ввести координаты снова")
         else: break
     closest_bar = calc.shortest_way(current_coords, data)
     out.output(closest_bar)
