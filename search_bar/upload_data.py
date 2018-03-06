e3e20a3cd094f0be38c3b9bed5e4f02928cb31a4
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-06 17:59:00 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -5,7 +5,7 @@ def read_bars(): # Вводимая директория файла json
     while True:
         try:
             while isf(path) !=  True:   # Проверка на правильность ввода директории и типа файла
-                path = "search_bar/data-2897-2017-12-21.json"
+                path = input("Введите директорию .json файла")
                 if isf(path) == False: print("Файл не найден, попробуйте ввести директорию файла заново")
                 else: break
                 with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла            
