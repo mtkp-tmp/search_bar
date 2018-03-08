79032513d3ff2cd5f36071905224e62becb4d307
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 20:57:00 +0300

    Update output_info.py

--- search_bar/output_info.py
+++ search_bar/output_info.py
@@ -1,3 +1,3 @@
 import locale
-locale.setlocale(locale.LC_ALL, "Russian Russia.l251")
+locale.setlocale(locale.LC_ALL, ('RU','UTF8'))
 def output(d : dict): print("{\n"+"\tНазвание ближайшего бара"+" : "+d["Название ближайшего бара"]+"\n\tАдрес бара"+" : "+d["Адрес бара"]+"\n\tКоличество сидячих мест"+" : "+str(d["Количество сидячих мест"])+"\n}")
