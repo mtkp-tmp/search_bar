55b051f88b8cf965842dae921887efe7806d8493
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 16:08:41 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -2,7 +2,7 @@ from json import load as l
 from os.path import isfile as isf
 def read_bars(): # Вводимая директория файла json
     path = input("Введите директорию .json файла") #"search_bar/data.json"  
-    with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла    
+    with open(path - "\r",'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла    
     '''
     while True:
         try:
