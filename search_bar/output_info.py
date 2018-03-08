22a335f72b7f238fea250ff9563d851264f71e81
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 21:54:37 +0300

    Update output_info.py

--- search_bar/output_info.py
+++ search_bar/output_info.py
@@ -1,3 +1,1 @@
-import sys, locale
-sys.setdefaultencoding(locale.getpreferredencoding())
 def output(d : dict): print("{\n"+"\tНазвание ближайшего бара"+" : "+d["Название ближайшего бара"]+"\n\tАдрес бара"+" : "+d["Адрес бара"]+"\n\tКоличество сидячих мест"+" : "+str(d["Количество сидячих мест"])+"\n}")
