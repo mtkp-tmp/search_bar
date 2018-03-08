fae7739ac14856bf7bb0f1a162f6a7ca5699d3c3
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 20:06:32 +0300

    Update output_info.py

--- search_bar/output_info.py
+++ search_bar/output_info.py
@@ -1,3 +1,3 @@
 from unicodedata import normalize as norm
-def output(d : dict): print("{\n"+"\tНазвание ближайшего бара"+" : "+norm('NFKD', d["Название ближайшего бара"]).encode('ASCII')+"\n\tАдрес бара"+" : "+d["Адрес бара"]+"\n\tКоличество сидячих мест"+" : "+str(d["Количество сидячих мест"])+"\n}")
+def output(d : dict): print("{\n"+"\tНазвание ближайшего бара"+" : "+norm('NFKD', d["Название ближайшего бара"]).decode('cp1251')+"\n\tАдрес бара"+" : "+d["Адрес бара"]+"\n\tКоличество сидячих мест"+" : "+str(d["Количество сидячих мест"])+"\n}")
 #unicodedata.normalize('NFKD', x).encode('ASCII', 'ignore')
