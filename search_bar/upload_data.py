9802a4beaddd4763771269cbf2b8017a6fd05597
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 21:24:50 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -2,11 +2,8 @@ from json import load as l
 def read_bars(): # Вводимая директория файла json
     try:
         path = input("Введите директорию .json файла") #"search_bar/data.json"  
-        path = path[0:len(path)-1] #удаление символа возврата каретки \r
-        if isf(path) == False: print("Файл не найден, попробуйте ввести директорию файла заново")
-        elif isf(path) == True: print("Файл найден!")
-        print("path = " + path)
-        with open(path,'r',encoding='utf-8') as f: return(l(f)) # Открытие файла    
+        #path = path[0:len(path)-1] #удаление символа возврата каретки \r
+        with open(path[0:len(path)-1],'r',encoding='utf-8') as f: return(l(f)) # Открытие файла    
     except OSError: print("Файл не найден!")
         
     '''
