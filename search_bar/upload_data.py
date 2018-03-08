c2b5453137e565759471c4a6ca1b24d2ee0e28cd
AlexOdlin <rockguitarist94alex@gmail.com>
2018-03-08 12:09:50 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -2,6 +2,8 @@ from json import load as l
 from os.path import isfile as isf
 def read_bars(): # Вводимая директория файла json
     path = "data.json"
+    with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла   
+    '''
     while True:
         try:
             while isf(path) !=  True:   # Проверка на правильность ввода директории и типа файла
@@ -11,3 +13,4 @@ def read_bars(): # Вводимая директория файла json
                 else: break
                 with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла            
         except: data = read_bars()
+     '''
