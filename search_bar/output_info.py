66d5d5fde48ec655c2d9ab82c8e79585450bb829
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-08 21:56:24 +0300

    Update output_info.py

--- search_bar/output_info.py
+++ search_bar/output_info.py
@@ -1,4 +1,1 @@
-import sys
-import codecs
-sys.stdout = codecs.getwriter('cp866')(sys.stdout,'replace')
 def output(d : dict): print("{\n"+"\tНазвание ближайшего бара"+" : "+d["Название ближайшего бара"]+"\n\tАдрес бара"+" : "+d["Адрес бара"]+"\n\tКоличество сидячих мест"+" : "+str(d["Количество сидячих мест"])+"\n}")
