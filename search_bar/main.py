7c76f60e99e14e25e01bcb72db9a4a334b21040f
KARLOV Andrey <TULAAA@yandex.ru>
2018-03-09 19:48:28 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,12 +1,1 @@
 #! Основной файл, выполняющий сборку проекта
-import upload_data
-import calculations
-import output_info
-
-
-def main():
-    coord1 = float(input("Введите первую координату:"))
-    coord2 = float(input("Введите вторую координату:"))
-    output_info.output_info(calculations.calculations(coord1, coord2, upload_data.upload_data()))
-if __name__ == "__main__":
-    main()
