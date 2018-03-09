daa92df89ef6bd8c140660c1fdd7522dd8d0fd73
KARLOV Andrey <TULAAA@yandex.ru>
2018-03-09 19:43:01 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,32 +1,12 @@
-#__________________________________________________________________________________________________________
-#Шаблон модуля main
-#__________________________________________________________________________________________________________
-import upload_data #Или |from upload_data import имя_функции| для импортирования только одной функции
+#! Основной файл, выполняющий сборку проекта
+import upload_data
 import calculations
 import output_info
-#вызовы функций из модулей:
-#имя_модуля.имя_функции()
 
+
+def main():
+    coord1 = float(input("Введите первую координату:"))
+    coord2 = float(input("Введите вторую координату:"))
+    output_info.output_info(calculations.calculations(coord1, coord2, upload_data.upload_data()))
 if __name__ == "__main__":
-  main()
-#__________________________________________________________________________________________________________
-#Черновик
-#__________________________________________________________________________________________________________
-import json
-import math
-
-'''coordX = input("Введите координаты X: ")
-coordY = input("Введите координаты Y: ")'''
-
-with open('data-2897-2017-12-21.json', 'r', encoding='windows-1251') as fileJSON:
-    data = json.load(fileJSON)
-
-print(data[0]['Name'])
-print(data[0]['Address'])
-print(data[0]['SeatsCount'])
-print(data[0]['geoData']['coordinates'][0])
-print(data[0]['geoData']['coordinates'][1])
-
-fileJSON.close()
-
-#Позже перенести в upload_data.py
+    main()
