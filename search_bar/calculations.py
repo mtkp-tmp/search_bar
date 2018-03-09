200472b94628f968e83902112c2a8bbe58345f85
KARLOV Andrey <TULAAA@yandex.ru>
2018-03-09 19:45:47 +0300

    Update calculations.py

--- search_bar/calculations.py
+++ search_bar/calculations.py
@@ -1,1 +1,9 @@
-
+#! Функция, выполняющая математические вычисления
+def calculations(coord1, coord2, data):
+    minimum = 99999.0
+    for bar in data:
+        distance = ((coord1-bar['geoData']['coordinates'][1]) ** 2)+((coord2-bar['geoData']['coordinates'][0]) ** 2)
+        if distance < minimum:
+            minimum = distance
+            closes_bar = bar
+    return closes_bar
