e36887d4dc72207bcd6a1d4d6fc2c2f49f5ea06d
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-11 01:14:12 +0300

    Update main.py

--- search_bar/main.py
+++ search_bar/main.py
@@ -1,9 +1,12 @@
-from json import load as l
-from os.path import isfile as isf
-def read_bars(): # Вводимая директория файла json
+import upload_data as upl, calculations as calc, output_info as out
+if __name__ == '__main__':
+    data = upl.read_bars()
     while True:
-        try:
-            path = input("Введите директорию .json файла").strip() #"search_bar/data.json"  
-            with open(path,'r') as f: return(l(f)) # Открытие файла и удаление символа возврата каретки \r 
-            if isf(path)==True: break
-        except OSError: print("Файл не найден!")
+        try: current_coords = { "Latitude_WGS84" : float(input("Введите долготу")), "Longitude_WGS84" : float(input("Введите широту")) }
+        except ValueError:
+            print("Введённые координаты не могут быть преобразованый в float, попробуйте ввести координаты снова")
+            try: current_coords
+            except NameError: current_coords = {}
+        if len(current_coords.keys()) == 2: break
+    closest_bar = calc.shortest_way(current_coords, data)
+    out.output(closest_bar)
