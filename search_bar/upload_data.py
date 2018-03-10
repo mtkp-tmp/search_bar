ba58bc28ac9be9c4e005962a71b90a3fac0e507e
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-11 01:22:30 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -5,7 +5,7 @@ def read_bars(): # Вводимая директория файла json
     while True:
         try:
             path = input("Введите директорию .json файла").strip() #"search_bar/data.json"  
-            with o("r", encoding='utf-8', errors='ignore') as f: return(l(f)) # Открытие файла и удаление символа возврата каретки \r 
+            with o(path, "r", encoding='utf-8', errors='ignore') as f: return(l(f)) # Открытие файла и удаление символа возврата каретки \r 
             if isf(path)==True: break
         except OSError: print("Файл не найден!")
         
