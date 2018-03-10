e5e5647fd6a1e6fcefb444d1ea77a89f7037e730
Seizz <36148013+Seizz@users.noreply.github.com>
2018-03-11 01:17:07 +0300

    Update upload_data.py

--- search_bar/upload_data.py
+++ search_bar/upload_data.py
@@ -1,18 +1,10 @@
 from json import load as l
+from os.path import isfile as isf
 def read_bars(): # Вводимая директория файла json
-    try:
-        path = input("Введите директорию .json файла").strip() #"search_bar/data.json"  
-        with open(path,'r') as f: return(l(f)) # Открытие файла и удаление символа возврата каретки \r 
-    except OSError: print("Файл не найден!")
-        
-    '''
     while True:
         try:
-            while isf(path) !=  True:   # Проверка на правильность ввода директории и типа файла
-                #path = input("Введите директорию .json файла")
-                print(path) #отладка travis
-                if isf(path) == False: print("Файл не найден, попробуйте ввести директорию файла заново")
-                else: break
-                with open(path,'r',encoding='windows-1251') as f: return(l(f)) # Открытие файла            
-        except: data = read_bars()
-   '''
+            path = input("Введите директорию .json файла").strip() #"search_bar/data.json"  
+            with open(path,'r') as f: return(l(f)) # Открытие файла и удаление символа возврата каретки \r 
+            if isf(path)==True: break
+        except OSError: print("Файл не найден!")
+        
